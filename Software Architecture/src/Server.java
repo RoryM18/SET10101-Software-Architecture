74986b28626db03e7361aae4f2a3bc507267cd2a
RoryM18 <71610882+RoryM18@users.noreply.github.com>
2023-10-01 22:55:12 +0100

    Add files via upload

---
+++ Software Architecture/src/Server.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server 
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			// First create the input from the keyboard
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Server Program");
+			
+			// Get the port to listen on
+			System.out.print("Enter port number to listen on: ");
+			String port_string = input.readLine();
+			
+			// The port number needs to be an int, so convert the String to an int
+			int port = Integer.parseInt(port_string);
+			
+			// Create a ServerSocket to listen on this address
+			ServerSocket server = new ServerSocket(port);
+			
+			// Accept an incoming client connection on the server socket
+			Socket sock = server.accept();
+			
+			// Create the output stream to the client
+			DataOutputStream network = new DataOutputStream(sock.getOutputStream());
+			
+			// Send message
+			network.writeUTF("Welcome " + sock.getInetAddress().getHostName() + ". We are " + new Date() + "\n");
+			
+			// Close sockets.  This will cause the client to exit
+			sock.close();
+			server.close();
+		}
+		catch (IOException ioe)
+		{
+			System.err.println("Error in I/O");
+			System.err.println(ioe.getMessage());
+			System.exit(-1);
+		}
+	}
+}
